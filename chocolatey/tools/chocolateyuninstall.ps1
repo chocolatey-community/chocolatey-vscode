6e43d512288570e1eb1581fa1fc98d3bce80ab43
AdmiringWorm <kim.nordmo@gmail.com>
2018-11-28 17:40:57 +0100

    (GH-20) Added new chocolatey package uninstall script

---
+++ chocolatey/tools/chocolateyuninstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+. "$toolsDir\Uninstall-VsCodeExtension.ps1"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  extensionName = 'gep13.chocolatey-vscode'
+}
+
+Uninstall-VsCodeExtension @packageArgs
