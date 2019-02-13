2a7d18874d73625af3221aa55aaea137d1da6c4a
Gary Ewan Park <gep13@gep13.co.uk>
2019-02-13 09:26:34 +0000

    (GH-70) Initial implementation of Language Server
    - This still needs some work, but it is at least working now
    - Single rule for templated values has been implemented
    - Question remains over how to get server into file system
    - Question remains over sending entire file to server

---
+++ src/Chocolatey.Language.Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OmniSharp.Extensions.LanguageServer.Server;
+
+namespace Chocolatey.Language.Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var options = new LanguageServerOptions()
+                .WithInput(Console.OpenStandardInput())
+                .WithOutput(Console.OpenStandardOutput())
+                .WithLoggerFactory(new LoggerFactory())
+                .AddDefaultLoggingProvider()
+                .WithMinimumLogLevel(LogLevel.Trace)
+                .WithServices(ConfigureServices)
+                .WithHandler<TextDocumentSyncHandler>()
+                .OnInitialize((s, _) => {
+                    var serviceProvider = (s as LanguageServer).Services;
+                    var bufferManager = serviceProvider.GetService<BufferManager>();
+                    var diagnosticsHandler = serviceProvider.GetService<DiagnosticsHandler>();
+
+                    // Hook up diagnostics
+                    bufferManager.BufferUpdated += (__, x) => diagnosticsHandler.PublishDiagnostics(x.Uri, bufferManager.GetBuffer(x.Uri));
+
+                    return Task.CompletedTask;
+                });
+
+            var server = await LanguageServer.From(options);
+
+            await server.WaitForExit;
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<BufferManager>();
+            services.AddSingleton<DiagnosticsHandler>();
+        }
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,6 @@
-import {window, commands, workspace, QuickPickItem} from "vscode";
+import { window, commands, workspace, QuickPickItem, ExtensionContext } from "vscode";
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
+import { Trace } from 'vscode-jsonrpc';
 import * as chocolateyCli from "./ChocolateyCliManager";
 import * as chocolateyOps from "./ChocolateyOperation";
 import * as path from "path";
@@ -7,13 +9,47 @@ import * as fs from "fs";
 var chocolateyManager : chocolateyCli.ChocolateyCliManager;
 var installed : boolean = false;
 
-export function activate(): void {
+export function activate(context: ExtensionContext): void {
     // register Commands
     commands.registerCommand("chocolatey.new", () => execute("new"));
     commands.registerCommand("chocolatey.pack", () => execute("pack"));
     commands.registerCommand("chocolatey.delete", () => deleteNupkgs());
     commands.registerCommand("chocolatey.push", () => execute("push"));
     commands.registerCommand("chocolatey.installTemplates", () => execute("installTemplates"));
+
+    let serverExe = 'dotnet';
+
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+    let serverOptions: ServerOptions = {
+        // TODO: For the time being, this path is hard-coded
+        // A decision has to be made about how the Language Server is going to be placed on the file server for execution
+        run: { command: serverExe, args: ['/Users/gep13/github/gep13/chocolatey-vscode/src/Chocolatey.Language.Server/bin/Debug/netcoreapp2.1/Chocolatey.Language.Server.dll'] },
+        debug: { command: serverExe, args: ['/Users/gep13/github/gep13/chocolatey-vscode/src/Chocolatey.Language.Server/bin/Debug/netcoreapp2.1/Chocolatey.Language.Server.dll'] }
+    }
+
+    // Options to control the language client
+    let clientOptions: LanguageClientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [
+            {
+                pattern: '**/*.nuspec',
+            }
+        ],
+        synchronize: {
+            configurationSection: 'nuspec',
+            fileEvents: workspace.createFileSystemWatcher('**/*.nuspec')
+        },
+    }
+
+    // Create the language client and start the client.
+    const client = new LanguageClient('nuspec', 'nuspec', serverOptions, clientOptions);
+    client.trace = Trace.Verbose;
+    let disposable = client.start();
+
+    // Push the disposable to the context's subscriptions so that the
+    // client can be deactivated on extension deactivation
+    context.subscriptions.push(disposable);
 }
 
 function deleteNupkgs():void {
