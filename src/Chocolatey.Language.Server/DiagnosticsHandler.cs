5f7f9f10ff4bd354a5776f464d55da3e4161a8fe
Gary Ewan Park <gep13@gep13.co.uk>
2019-02-13 22:29:33 +0000

    (GH-70) Refactored to implement validations from interface
    - That way, all validations can be loaded at once
    - Based on conversation in GitHub issue

--- src/Chocolatey.Language.Server/DiagnosticsHandler.cs
+++ src/Chocolatey.Language.Server/DiagnosticsHandler.cs
@@ -9,7 +9,6 @@ using Microsoft.Language.Xml;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 using Buffer = Microsoft.Language.Xml.Buffer;
-using DiagnosticSeverity = OmniSharp.Extensions.LanguageServer.Protocol.Models.DiagnosticSeverity;
 
 namespace Chocolatey.Language.Server
 {
@@ -17,17 +16,22 @@ namespace Chocolatey.Language.Server
     {
         private readonly ILanguageServer _router;
         private readonly BufferManager _bufferManager;
-        private static readonly IReadOnlyCollection<string> TemplatedValues = new []
-        {
-            "__replace",
-            "space_separated",
-            "tag1"
-        };
+        private IList<INuSpecRule> _rules = new List<INuSpecRule>();
 
         public DiagnosticsHandler(ILanguageServer router, BufferManager bufferManager)
         {
             _router = router;
             _bufferManager = bufferManager;
+
+            var typeLocator = new TypeLocator();
+            foreach (var nuspecRule in typeLocator.GetTypesThatInheritOrImplement<INuSpecRule>().OrEmptyListIfNull())
+            {
+                var rule = Activator.CreateInstance(nuspecRule) as INuSpecRule;
+                if (rule != null)
+                {
+                    _rules.Add(rule);
+                }
+            }
         }
 
         public void PublishDiagnostics(Uri uri, Buffer buffer)
@@ -37,91 +41,16 @@ namespace Chocolatey.Language.Server
             var textPositions = new TextPositions(text);
             var diagnostics = new List<Diagnostic>();
 
-            diagnostics.AddRange(NuspecDoesNotContainTemplatedValuesRequirement(syntaxTree, textPositions));
-            diagnostics.AddRange(NuspecDescriptionLengthValidation(syntaxTree, textPositions));
+            foreach (var rule in _rules.OrEmptyListIfNull())
+            {
+                diagnostics.AddRange(rule.Validate(syntaxTree, textPositions));
+            }
 
             _router.Document.PublishDiagnostics(new PublishDiagnosticsParams
             {
                 Uri = uri,
                 Diagnostics = diagnostics
             });
-        }
-
-        /// <summary>
-        ///   Handler to validate that no templated values remain in the nuspec.
-        /// </summary>
-        /// <seealso href="https://github.com/chocolatey/package-validator/blob/master/src/chocolatey.package.validator/infrastructure.app/rules/NuspecDoesNotContainTemplatedValuesRequirement.cs">Package validator requirement for templated values.</seealso>
-        private IEnumerable<Diagnostic> NuspecDoesNotContainTemplatedValuesRequirement(XmlDocumentSyntax syntaxTree, TextPositions textPositions)
-        {
-            foreach (var node in syntaxTree.DescendantNodesAndSelf().OfType<XmlTextSyntax>())
-            {
-                if (!TemplatedValues.Any(x => node.Value.Contains(x, StringComparison.OrdinalIgnoreCase)))
-                {
-                    continue;
-                }
-
-                var range = textPositions.GetRange(node.Start, node.End);
-
-                yield return new Diagnostic {
-                    Message = "Templated value which should be removed",
-                    Severity = DiagnosticSeverity.Error,
-                    Range = range
-                };
-            }
-        }
-
-        /// <summary>
-        ///   Handler to validate the length of description in the package metadata.
-        /// </summary>
-        /// <seealso href="https://github.com/chocolatey/package-validator/blob/master/src/chocolatey.package.validator/infrastructure.app/rules/DescriptionRequirement.cs">Package validator requirement for description.</seealso>
-        /// <seealso href="https://github.com/chocolatey/package-validator/blob/master/src/chocolatey.package.validator/infrastructure.app/rules/DescriptionWordCountMaximum4000Requirement.cs">Package validator maximum length requirement for description.</seealso>
-        /// <seealso href="https://github.com/chocolatey/package-validator/blob/master/src/chocolatey.package.validator/infrastructure.app/rules/DescriptionWordCountMinimum30Guideline.cs">Package validator minimum length guideline for description.</seealso>
-        private IEnumerable<Diagnostic> NuspecDescriptionLengthValidation(XmlDocumentSyntax syntaxTree, TextPositions textPositions)
-        {
-            var descriptionElement = syntaxTree.DescendantNodes().OfType<XmlElementSyntax>().FirstOrDefault(x => string.Equals(x.Name, "description", StringComparison.OrdinalIgnoreCase));
-
-            if (descriptionElement == null)
-            {
-                yield return new Diagnostic {
-                    Message = "Description is required. See https://github.com/chocolatey/package-validator/wiki/DescriptionNotEmpty",
-                    Severity = DiagnosticSeverity.Error,
-                    Range = textPositions.GetRange(0, syntaxTree.End)
-                };
-                yield break;
-            }
-
-            var descriptionLength = descriptionElement.GetContentValue().Trim().Length;
-
-            if (descriptionLength == 0)
-            {
-                var range = textPositions.GetRange(descriptionElement.StartTag.End, descriptionElement.EndTag.Start);
-
-                yield return new Diagnostic {
-                    Message = "Description is required. See https://github.com/chocolatey/package-validator/wiki/DescriptionNotEmpty",
-                    Severity = DiagnosticSeverity.Error,
-                    Range = range
-                };
-            }
-            else if (descriptionLength <= 30)
-            {
-                var range = textPositions.GetRange(descriptionElement.StartTag.End, descriptionElement.EndTag.Start);
-
-                yield return new Diagnostic {
-                    Message = "Description should be sufficient to explain the software. See https://github.com/chocolatey/package-validator/wiki/DescriptionCharacterCountMinimum",
-                    Severity = DiagnosticSeverity.Warning,
-                    Range = range
-                };
-            }
-            else if (descriptionLength > 4000)
-            {
-                var range = textPositions.GetRange(descriptionElement.StartTag.End, descriptionElement.EndTag.Start);
-
-                yield return new Diagnostic {
-                    Message = "Description should not exceed 4000 characters. See https://github.com/chocolatey/package-validator/wiki/DescriptionCharacterCountMaximum",
-                    Severity = DiagnosticSeverity.Error,
-                    Range = range
-                };
-            }
         }
     }
 }
