e87ed731bb9f8547be0bbc52b8def7fee83ae829
AdmiringWorm <kim.nordmo@gmail.com>
2019-03-02 17:32:15 +0100

    (GH-137) Added dependency class to store a single dependency

---
+++ src/Chocolatey.Language.Server/Models/Dependency.cs
@@ -0,0 +1,18 @@
+﻿namespace Chocolatey.Language.Server.Models
+{
+    /// <summary>
+    /// Represents a single package dependency. This class cannot be inherited.
+    /// </summary>
+    public sealed class Dependency
+    {
+        /// <summary>
+        /// Gets or sets the identifier of the dependency.
+        /// </summary>
+        public MetaValue<string> Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version range of the dependency.
+        /// </summary>
+        public MetaValue<string> VersionRange { get; set; }
+    }
+}
