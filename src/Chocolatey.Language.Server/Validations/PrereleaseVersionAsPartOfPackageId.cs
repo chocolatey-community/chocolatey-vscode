6dd652b7ec51170bd88d1b8611961dd808b9a0c1
AdmiringWorm <kim.nordmo@gmail.com>
2019-03-08 17:51:30 +0100

    (GH-70) Implemented validation rule to check if package id contains pre-release tags

---
+++ src/Chocolatey.Language.Server/Validations/PrereleaseVersionAsPartOfPackageId.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Chocolatey.Language.Server.Extensions;
+using Chocolatey.Language.Server.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+
+namespace Chocolatey.Language.Server.Validations
+{
+    /// <summary>
+    ///   Handler to validate the id does not contain a pre-release tag (alpha, beta, prerelease).
+    /// </summary>
+    /// <seealso href="https://github.com/chocolatey/package-validator/blob/master/src/chocolatey.package.validator/infrastructure.app/rules/PrereleaseVersionAsPartOfPackageIdRequirement.cs">Package validator rule for package id does not contain pre-release tags.</seealso>
+    public sealed class PrereleaseVersionAsPartOfPackageId : NuspecRuleBase
+    {
+        private const string VALIDATION_MESSAGE = "The package id includes a prerelease version name which should be included only in the version of the package.";
+
+        public override string Id => "CHOCO0010";
+
+        public override string DocumentationUrl => $"https://gep13.github.io/chocolatey-vscode/docs/rules/{Id}";
+
+        public override ValidationType ValidationType => ValidationType.Requirement;
+
+        public override IEnumerable<Diagnostic> Validate(Package package)
+        {
+            if (package.Id.ContainsAny("alpha", "beta", "prerelease"))
+            {
+                yield return CreateDiagnostic(package.Id, VALIDATION_MESSAGE);
+            }
+        }
+    }
+}
