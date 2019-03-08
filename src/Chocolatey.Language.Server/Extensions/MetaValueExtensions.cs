c1822ce79eeddd41a48c2c684ecf99afa73a51b8
AdmiringWorm <kim.nordmo@gmail.com>
2019-03-08 17:31:20 +0100

    (GH-70) Added extension helper class to work with Meta Values

---
+++ src/Chocolatey.Language.Server/Extensions/MetaValueExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Chocolatey.Language.Server.Models;
+
+namespace Chocolatey.Language.Server.Extensions
+{
+    /// <summary>
+    /// Helper class to make it easier to test out values in the Meta Value class.
+    /// </summary>
+    public static class MetaValueExtensions
+    {
+        public static bool EndsWith(this MetaValue<string> source, string text)
+        {
+            return !source.IsMissing && source.Value.EndsWith(text, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
