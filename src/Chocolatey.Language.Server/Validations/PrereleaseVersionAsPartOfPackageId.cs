e9f46fff127aef54a23dae029b6d75cbb110ad02
Gary Ewan Park <gep13@gep13.co.uk>
2019-03-09 18:05:29 +0000

    (doc) Changed case to match what is in use

--- src/Chocolatey.Language.Server/Validations/PrereleaseVersionAsPartOfPackageId.cs
+++ src/Chocolatey.Language.Server/Validations/PrereleaseVersionAsPartOfPackageId.cs
@@ -13,7 +13,7 @@ namespace Chocolatey.Language.Server.Validations
     {
         private const string VALIDATION_MESSAGE = "The package id includes a prerelease version name which should be included only in the version of the package.";
 
-        public override string Id => "CHOCO0010";
+        public override string Id => "choco0010";
 
         public override string DocumentationUrl => $"https://gep13.github.io/chocolatey-vscode/docs/rules/{Id}";
 
